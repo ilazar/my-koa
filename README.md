bf3943ece4caabd0f5e55d9cf567dea708504aac
Ioan Lazar <ilazar@8x8.com>
2019-05-06 19:23:58 +0300

    02

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Changelog
 
 - 01 Configure backpack-core
+- 02 Use http & url modules
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import http from 'http';
+import url from 'url';
+
+const handler = (req, res) => {
+  const q = url.parse(req.url, true).query;
+  const name = q.name || 'World';
+  res.write(`Hello ${name}!`);
+  res.end();
+};
+
+const server = http.createServer(handler);
+
+const port = 3000;
+
+server.listen({ port }, () => {
+  console.log(`Server started on port ${port}`);
+});
