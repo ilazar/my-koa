810f022e9a41f83b397a520fee7cfce420676d4d
Ioan Lazar <ilazar@8x8.com>
2019-05-10 12:29:24 +0300

    04p

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 01 Configure backpack-core
 - 02 Use http & url modules
 - 03 Create app to hide http details
+- 04 Add middlewares
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import App from './my-koa';
+import bodyParser from './my-koa';
 
 const app = new App();
+
+const exceptionHandler = async (ctx, next) => {
+  try {
+    return await next();
+  } catch (err) {
+    ctx.body = { message: err.message || 'Unexpected error.' };
+    ctx.status = err.status || 500;
+  }
+};
+
+const timingLogger = async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  console.log(`${ctx.request.method} ${ctx.request.url} => ${ctx.response.status}, ${Date.now() - start}ms`);
+};
 
 const sayHello = ctx => {
   const { query } = ctx.request;
@@ -8,6 +24,9 @@ const sayHello = ctx => {
   ctx.response.body = `Hello ${name}!`;
 };
 
+app.use(exceptionHandler);
+app.use(timingLogger);
+app.use(bodyParser());
 app.use(sayHello);
 
 const port = 3000;
